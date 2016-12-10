ed907bd0a6407de9563a3ff07386c1e61ab895c6
Thomas Breuss <thomasbreuss@gmx.ch>
2016-12-10 12:43:15 +0100

    Added application properties

--- src/main/java/ch/tebe/App.java
+++ src/main/java/ch/tebe/App.java
@@ -1,5 +1,7 @@
 package ch.tebe;
 
+import java.util.ResourceBundle;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,10 @@ public class App
 {
     public static void main( String[] args )
     {
+        ResourceBundle rb = ResourceBundle.getBundle("ApplicationResources");
+        String appname = rb.getString("application.name");
+        String version = rb.getString("application.version");
+        System.out.println( "This is " + appname + " (Version " + version +")");
         System.out.println( "Hello World!" );
     }
 }
